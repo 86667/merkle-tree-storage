8351b35273d570ed38d6b41098ff17ee06ffbce3
tomosTrustology <tomos.wootton@bitpanda.com>
2024-03-15 17:46:39 +0000

    verify()

--- merkle_tree/src/lib.rs
+++ merkle_tree/src/lib.rs
@@ -1,11 +1,12 @@
 use std::fmt::Display;
-use sha2::{digest::block_buffer::Error, Digest, Sha256};
+use sha2::{Digest, Sha256};
 
 #[derive(Debug)]
 pub struct MerkleTree {
     /// Binary tree represented as a 2-dimensional vector in which the outer vector represents each row and inner vector represents the nodes on that row   
     /// Note that leaf nodes are stored in row 0 and the root node in row (tree.len()-1)
     pub tree: Vec<Vec<String>>,
+    pub leaves: usize
 }
 
 impl Display for MerkleTree {
@@ -23,19 +24,19 @@ impl Display for MerkleTree {
 
 impl MerkleTree {
     /// Take a list of leaf hashes and build full merkle tree
-    pub fn build(leaves: Vec<String>) -> MerkleTree {
+    pub fn build(leaves: &Vec<String>) -> MerkleTree {
         let depth: usize = MerkleTree::find_depth(leaves.len());
         
         // Row 0
         let mut tree: Vec<Vec<String>> = Vec::with_capacity(depth);
-        tree.push(leaves);
+        tree.push(leaves.clone());
 
         // Build each row of Merkle tree
         for row in 0..depth-1 {
             let mut next_row: Vec<String> = vec!();
             // Hash concaternation of pairs of items on current row to build next row
             for i in (0..tree[row].len()).step_by(2) {
-                let concat = MerkleTree::concat_string(&tree[row][i],&tree[row][i+1]);
+                let concat = concat_string(&tree[row][i],&tree[row][i+1]);
                 next_row.push(hash(concat.as_ref()));
             }
             // println!("next row: {:?}", next_row);
@@ -43,7 +44,8 @@ impl MerkleTree {
         }
 
         MerkleTree {
-            tree
+            tree,
+            leaves: leaves.len()
         }
     } 
 
@@ -74,16 +76,14 @@ impl MerkleTree {
 
     /// Create a vector of hashes which are the nodes required to rebuild the root hash from the queried index
     pub fn prove(&self, index: usize) -> Vec<String> {
-        if index > (self.tree.len() - 1) {
+        if index > (self.leaves) {
             panic!("Index too large. Tree contains {} nodes.", self.tree.len())
         }
         // First find the indicies of each node in path from leaf to root
         let path_to_root = self.find_path_leaf_to_root(index);
-        println!("{:?}", path_to_root);
 
         // Next find the sibling node to each node in the path to root
         let sibling_path: Vec<usize> = path_to_root.into_iter().map(|x| MerkleTree::find_node_sibling(x)).collect();
-        println!("{:?}", sibling_path);
 
         // The proof vector then is a hash from each row at index in sibling_path vector
         let mut proof = Vec::new();
@@ -91,10 +91,6 @@ impl MerkleTree {
             proof.push(self.tree[row][sibling_path[row]].clone())
         }
         proof
-    }
-
-    fn concat_string(string1: &String, string2: &String) -> String {
-        string1.clone() + &string2
     }
 
     fn find_depth(num_items: usize) -> usize {
@@ -109,6 +105,29 @@ pub fn hash(message: &[u8]) -> String {
     base16ct::lower::encode_string(&hash)
 }
 
+/// Take two Strings and copy them into a new String which is a concaternation of them  
+/// Strings are ordered lexigraphically
+fn concat_string(string1: &String, string2: &String) -> String {
+    if string1 > string2 {
+        return string1.clone() + &string2;
+    }
+    string2.clone() + &string1
+}
+
+/// Take a root hash, item hash and proof and return true if proof validates the item hash in merkle tree with given root
+pub fn verify(root_hash: &String, item_hash: &String, proof: &Vec<String>) -> bool {
+    // TODO: client side: First hash the file and verify first hash
+    // if file_hash != proof[0] {
+    //     false
+    // }
+
+    let mut current_hash = item_hash.clone();
+    for i in 0..proof.len() {
+        current_hash = hash(concat_string(&current_hash, &proof[i].clone()).as_ref());
+    } 
+    current_hash == *root_hash
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -118,30 +137,53 @@ mod tests {
     fn playground() {
         let inputs: Vec<String> = vec!(String::from("0"),String::from("1"),String::from("2"),String::from("3"));
         let hashes: Vec<String> = inputs.into_iter().map(|x: String| hash(x.as_ref())).collect();
-        let merkle_tree = MerkleTree::build(hashes);
+        let merkle_tree = MerkleTree::build(&hashes);
         println!("{}", merkle_tree);
+
+        let proof = merkle_tree.prove(0usize);
+        println!("proof: {:?}", proof);
+
+        let res = verify(&merkle_tree.tree[merkle_tree.tree.len()-1][0], &hashes[0], &proof);
+        assert_eq!(res, true);
     }
 
     #[test]
-    fn test_build() {
+    fn test_build_and_verify() {
         let inputs: Vec<String> = vec!(String::from("0"),String::from("1"),String::from("2"),String::from("3"));
         let hashes: Vec<String> = inputs.into_iter().map(|x| hash(x.as_ref())).collect();
-        let merkle_tree = MerkleTree::build(hashes);
-        // Generated manually
+        let merkle_tree = MerkleTree::build(&hashes);
+        // Data generated manually
         assert_eq!(merkle_tree.tree[0][0], String::from("5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9"));
         assert_eq!(merkle_tree.tree[0][1], String::from("6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b")); 
         assert_eq!(merkle_tree.tree[0][2], String::from("d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35")); 
         assert_eq!(merkle_tree.tree[0][3], String::from("4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce")); 
-        assert_eq!(merkle_tree.tree[1][0], String::from("fa13bb36c022a6943f37c638126a2c88fc8d008eb5a9fe8fcde17026807feae4")); 
+        assert_eq!(merkle_tree.tree[1][0], String::from("dbdbf4fb684471f421fb255100e433c77fd1aac71c7a3739e9897168aec67ec1")); 
         assert_eq!(merkle_tree.tree[1][1], String::from("70311d9d203b2d7e4ff70d7fce219f82a4fcf73a110dc80187dfefb7c6e4bb87"));
-        assert_eq!(merkle_tree.tree[2][0], String::from("862532e6a3c9aafc2016810598ed0cc3025af5640db73224f586b6f1138385f4"));
+        assert_eq!(merkle_tree.tree[2][0], String::from("a48572b1744e5e3f3473c9eaa91f73be774712d2b207c34eb537023af0ec6528"));
+
+        let root_hash = merkle_tree.tree[merkle_tree.tree.len()-1][0].clone();
+
+        // Build all proof and verify
+        let mut proof = merkle_tree.prove(0usize);
+        assert!(verify(&root_hash, &hashes[0], &proof));
+        proof = merkle_tree.prove(1usize);
+        assert!(verify(&root_hash, &hashes[1], &proof));
+        proof = merkle_tree.prove(2usize);
+        assert!(verify(&root_hash, &hashes[2], &proof));
+        proof = merkle_tree.prove(3usize);
+        assert!(verify(&root_hash, &hashes[3], &proof));
+    }
+
+    #[test]
+    fn test_build_leaves_not_power_of_2() {
+
     }
 
     #[test]
     fn test_prove() {
         let inputs: Vec<String> = vec!(String::from("0"),String::from("1"),String::from("2"),String::from("3"));
         let hashes: Vec<String> = inputs.into_iter().map(|x| hash(x.as_ref())).collect();
-        let merkle_tree = MerkleTree::build(hashes);
+        let merkle_tree = MerkleTree::build(&hashes);
         assert_eq!(merkle_tree.prove(0), Vec::from([merkle_tree.tree[0][1].clone(), merkle_tree.tree[1][1].clone()]));
         assert_eq!(merkle_tree.prove(1), Vec::from([merkle_tree.tree[0][0].clone(), merkle_tree.tree[1][1].clone()]));
         assert_eq!(merkle_tree.prove(2), Vec::from([merkle_tree.tree[0][3].clone(), merkle_tree.tree[1][0].clone()]));
@@ -152,17 +194,18 @@ mod tests {
     #[test]
     fn test_find_path_leaf_to_root() {
         let merkle_tree = MerkleTree {
-            tree: vec![Vec::new();4]
+            tree: vec![Vec::new();4],
+            leaves: 4
         };
         // test data generated manually
-        assert_eq!(merkle_tree.find_path_leaf_to_root(0), Vec::from([0,0,0,0]));
-        assert_eq!(merkle_tree.find_path_leaf_to_root(1), Vec::from([1,0,0,0]));
-        assert_eq!(merkle_tree.find_path_leaf_to_root(2), Vec::from([2,1,0,0]));
-        assert_eq!(merkle_tree.find_path_leaf_to_root(3), Vec::from([3,1,0,0]));
-        assert_eq!(merkle_tree.find_path_leaf_to_root(4), Vec::from([4,2,1,0]));
-        assert_eq!(merkle_tree.find_path_leaf_to_root(5), Vec::from([5,2,1,0]));
-        assert_eq!(merkle_tree.find_path_leaf_to_root(6), Vec::from([6,3,1,0]));
-        assert_eq!(merkle_tree.find_path_leaf_to_root(7), Vec::from([7,3,1,0]));
+        assert_eq!(merkle_tree.find_path_leaf_to_root(0), Vec::from([0,0,0]));
+        assert_eq!(merkle_tree.find_path_leaf_to_root(1), Vec::from([1,0,0]));
+        assert_eq!(merkle_tree.find_path_leaf_to_root(2), Vec::from([2,1,0]));
+        assert_eq!(merkle_tree.find_path_leaf_to_root(3), Vec::from([3,1,0]));
+        assert_eq!(merkle_tree.find_path_leaf_to_root(4), Vec::from([4,2,1]));
+        assert_eq!(merkle_tree.find_path_leaf_to_root(5), Vec::from([5,2,1]));
+        assert_eq!(merkle_tree.find_path_leaf_to_root(6), Vec::from([6,3,1]));
+        assert_eq!(merkle_tree.find_path_leaf_to_root(7), Vec::from([7,3,1]));
     }
 
     #[test]
